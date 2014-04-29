a676530bacd88bb670b2802a5ac72ddafca1e4a3
Jan Dolinár <dolik.rce@gmail.com>
2014-04-29 21:44:05 +0200

    displaying information about builders (fixes #6)

--- src/Watchdog/Server/Sql.h
+++ src/Watchdog/Server/Sql.h
@@ -23,9 +23,17 @@ inline SqlVal If(const SqlBool& b, const SqlVal& trueval, const SqlVal& falseval
 	return SqlFunc("if", SqlSet(SqlVal(~b, SqlS::HIGH), trueval, falseval));
 }
 
+inline SqlVal SqlNull() {
+	return SqlVal("NULL", SqlS::HIGH);
+}
+
 inline SqlVal CountIf(const SqlVal& val, const SqlBool& b){
 	//SqlBool cond = (b && SqlBool(b(SqlS::COMP) + " is not NULL", SqlS::COMP));
 	return SqlFunc("sum", If(b, val, 0));
+}
+
+inline SqlVal CountDistinctIf(const SqlVal& val, const SqlBool& b){
+	return Count(Distinct(If(b, val, SqlNull())));
 }
 
 inline SqlVal Concat(const SqlSet& set){
